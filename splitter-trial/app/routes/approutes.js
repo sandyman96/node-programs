7170d6e1d99855a6d06290d6ee7f4c53f75af97e
sandeepraman <sandeepraman96@gmail.com>
2020-02-26 16:36:01 +0530

    fixing .gitignore

--- splitter-trial/app/routes/approutes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var appController = require("../controller/appController");
-const BODY_PARSER_VAR = require("body-parser");
-
-
-module.exports = function (expressApp){
-    expressApp.use(BODY_PARSER_VAR.json() );
-    expressApp.route("/register").post(appController.registerUser);
-}
