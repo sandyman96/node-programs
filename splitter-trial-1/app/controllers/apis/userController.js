47166046f1cf017d7b7985f915731952e6c67df0
sandeepraman <sandeepraman96@gmail.com>
2020-03-03 09:17:29 +0530

    added home files

---
+++ splitter-trial-1/app/controllers/apis/userController.js
@@ -0,0 +1,28 @@
+/********
+* user.js file (controllers/apis)
+********/
+
+
+const express = require('express');
+const userService = require('../../services/users/userService');
+let router = express.Router();
+
+router.get('/users', userService.getUsers);
+
+router.post('/users/register', userService.createUsers );
+
+// router.get('/:id', userService.getUserById);
+
+// router.post('/', userService.createUser);
+
+// router.put('/:id', userService.updateUser);
+
+// router.delete('/:id', userService.deleteUser);
+
+module.exports = router;
+
+
+
+/********
+* user.js file (controllers/apis)
+********/
---
+++ splitter-trial-1/app/models/config/config.js
@@ -0,0 +1,10 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require('../../configs/db');
+console.log(db.user + db.password + db.host);
+
+const sequelize = new Sequelize(db.database, db.user, db.password, {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = {sequelize:sequelize, DataTypes:DataTypes};
